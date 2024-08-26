e31ceb638ccac9d3475f1c4653d22a23f944f4b5
Atena Razm <atenarazm@gmail.com>
2024-08-26 13:30:53 -0700

    fetching pizza data and displaying in MenuItem

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,5 +1,8 @@
+import { formatCurrency } from '../../utils/helpers';
+
 function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  console.log(id, name, unitPrice, ingredients, soldOut, imageUrl);
 
   return (
     <li>
