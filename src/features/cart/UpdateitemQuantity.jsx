347db88421079e9062747026502e7a516229585c
Atena Razm <atenarazm@gmail.com>
2024-08-28 10:37:43 -0700

    current quantity added to cart items

--- src/features/cart/UpdateitemQuantity.jsx
+++ src/features/cart/UpdateitemQuantity.jsx
@@ -2,17 +2,18 @@ import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
 import { decreaseItemQuantity, increaseItemQuantity } from './cartSlice';
 
-function UpdateitemQuantity({ pizzaId }) {
+function UpdateitemQuantity({ pizzaId, currentQuantity }) {
   const dispatch = useDispatch();
 
   return (
-    <div className="md-:gap-3 flex items-center gap-1">
+    <div className="md-:gap-3 flex items-center gap-2">
       <Button
         type="round"
         onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
       >
         -
       </Button>
+      <span className="text-sm font-medium">{currentQuantity}</span>
       <Button
         type="round"
         onClick={() => dispatch(increaseItemQuantity(pizzaId))}
