1344a721f465d3d6c7901423631e59358a491d19
Atena Razm <atenarazm@gmail.com>
2024-08-27 20:26:58 -0700

    total quantity & price appear in case there is any pizza added to the cart

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -7,6 +7,8 @@ function CartOverview() {
   const totalQuantity = useSelector(getTotalCartQuantity);
   const totalPrice = useSelector(getTotalCartPrice);
 
+  if (!totalQuantity) return null;
+
   return (
     <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
