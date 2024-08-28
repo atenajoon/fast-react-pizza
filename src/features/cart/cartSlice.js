6265344ef1e7a704e216782d8ba54fc195c1ffd0
Atena Razm <atenarazm@gmail.com>
2024-08-27 20:24:37 -0700

    total price and quantity come from cartSlice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -48,4 +48,10 @@ export const {
   clearCart,
 } = cartSlice.actions;
 
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
 export default cartSlice.reducer;
