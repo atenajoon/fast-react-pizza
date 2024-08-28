14ed72f09d6db98cfddb2f66d7b769c702bae42f
Atena Razm <atenarazm@gmail.com>
2024-08-27 20:34:52 -0700

    clearCart works

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -2,36 +2,13 @@ import { Link } from 'react-router-dom';
 import LinkButton from '../../ui/LinkButton';
 import Button from '../../ui/Button';
 import CartItem from './CartItem';
-import { useSelector } from 'react-redux';
-import { getCart, getUsername } from './cartSlice';
-
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, getCart, getUsername } from './cartSlice';
 
 function Cart() {
   const cart = useSelector(getCart);
   const username = useSelector(getUsername);
+  const dispatch = useDispatch();
 
   return (
     <div className="px-4 py-3">
@@ -50,7 +27,9 @@ function Cart() {
           Order pizzas
         </Button>
 
-        <Button type="secondary">Clear cart</Button>
+        <Button type="secondary" onClick={() => dispatch(clearCart())}>
+          Clear cart
+        </Button>
       </div>
     </div>
   );
