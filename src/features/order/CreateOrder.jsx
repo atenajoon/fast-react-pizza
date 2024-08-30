905adcbad40641e32549222e61edce0abbcc8b5b
Atena Razm <atenarazm@gmail.com>
2024-08-29 21:20:25 -0700

    a hacky way to empty cart after order submission

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -4,7 +4,8 @@ import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
 import EmptyCart from '../cart/EmptyCart';
-import { getCart } from '../cart/cartSlice';
+import { clearCart, getCart } from '../cart/cartSlice';
+import store from '../../store';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -107,8 +108,10 @@ export async function action({ request }) {
   if (Object.keys(errors).length > 0) return errors;
 
   // If everything is okay, create new order and redirect
-
   const newOrder = await createOrder(order);
+
+  //  DO NOT Overuse this hacky way:
+  store.dispatch(clearCart());
 
   return redirect(`/order/${newOrder.id}`);
 }
