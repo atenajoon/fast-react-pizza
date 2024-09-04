acc0d23f3ccac5481855741f2d710969677ee722
Atena Razm <atenarazm@gmail.com>
2024-09-04 11:23:32 -0700

    priority data is updated

--- src/features/order/UpdateOrder.jsx
+++ src/features/order/UpdateOrder.jsx
@@ -1,5 +1,6 @@
 import { useFetcher } from 'react-router-dom';
 import Button from '../../ui/Button';
+import { updateOrder } from '../../services/apiRestaurant';
 
 function UpdateOrder({ order }) {
   const fetcher = useFetcher();
@@ -14,6 +15,8 @@ function UpdateOrder({ order }) {
 export default UpdateOrder;
 
 export async function action({ request, params }) {
-  console.log('updated');
+  const data = { priority: true };
+  await updateOrder(params.orderId, data);
+
   return null;
 }
