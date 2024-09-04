65a8da2483dcac92bfed3871a992e687434ee824
Atena Razm <atenarazm@gmail.com>
2024-09-04 11:09:51 -0700

    the rest of the previous commit

--- src/features/order/UpdateOrder.jsx
+++ src/features/order/UpdateOrder.jsx
@@ -1,7 +1,19 @@
+import { useFetcher } from 'react-router-dom';
 import Button from '../../ui/Button';
 
 function UpdateOrder({ order }) {
-  return <Button type="primary">Make priority</Button>;
+  const fetcher = useFetcher();
+
+  return (
+    <fetcher.Form method="PATCH" className="text-right">
+      <Button type="primary">Make priority</Button>
+    </fetcher.Form>
+  );
 }
 
 export default UpdateOrder;
+
+export async function action({ request, params }) {
+  console.log('updated');
+  return null;
+}
